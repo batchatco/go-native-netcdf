f77052d03c31c29c437ed2a9c698ae9752cf0610
bat <bradley.taylor@gmail.com>
2018-08-10 09:15:25 -0700

    Add HDF5 logger function as placeholder

--- netcdf/hdf5/hdf5.go
+++ netcdf/hdf5/hdf5.go
@@ -4,10 +4,20 @@ import (
 	"errors"
 
 	"github.com/batchatco/go-native-netcdf/netcdf/api"
+	"github.com/batchatco/go-native-netcdf/netcdf/util"
 )
 
 var ErrNotImplemented = errors.New("HDF5 is not implemented yet")
 
+var (
+	logger = util.NewLogger()
+	log    = "don't use the log package" // prevents usage of standard log package
+)
+
 func NewHDF5(fname string) (nc api.Group, err error) {
 	return nil, ErrNotImplemented
+}
+
+func SetLogLevel(level int) {
+	logger.SetLogLevel(level)
 }

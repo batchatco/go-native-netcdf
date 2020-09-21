24409dd19f9a42e7fd984501ee8b4a5e9bafa7d5
Bradley Taylor <bradley.taylor@gmail.com>
2020-09-21 16:13:30 -0700

    move some readers into rafile.go

--- netcdf/hdf5/rafile.go
+++ netcdf/hdf5/rafile.go
@@ -1,12 +1,25 @@
 package hdf5
 
+// Various readers here
+
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
 	"io"
 	"sync"
+
+	"github.com/batchatco/go-thrower"
 )
 
+// remReader remembers the count (used) and remaining bytes
+type remReader interface {
+	io.Reader
+	Count() int64
+	Rem() int64
+}
+
+// refCountedFile allows multiple readers on the same file handle
 type refCountedFile struct {
 	file     io.ReadSeeker
 	refCount int
@@ -19,11 +32,44 @@ type raFile struct {
 	seekPointer int64
 }
 
+// resetreader takes a file and a size (or a byte array), and
+// returns a remReader for it.
 type resetReader struct {
 	remReader
 	lr    *io.LimitedReader
 	size  int64
 	bytes []byte
+}
+
+// unshuffleReader implements the shuffle algorithm
+type unshuffleReader struct {
+	r            io.Reader
+	b            []byte
+	size         uint64
+	shuffleParam uint32
+}
+
+// newFletcher32reader creates a reader implemting the fletcher32 algorithm.
+// TODO: make this streaming instead of reading bytes up front.
+func newFletcher32Reader(r io.Reader, size uint64) remReader {
+	assert(size >= 4, "bad size for fletcher")
+	b := make([]byte, size-4)
+	read(r, b)
+	var checksum uint32
+	binary.Read(r, binary.LittleEndian, &checksum)
+	bf := newResetReaderFromBytes(b)
+	values := make([]uint16, len(b)/2)
+	binary.Read(bf, binary.BigEndian, values)
+	if len(b)%2 == 1 {
+		last := uint16(b[len(b)-1])
+		values = append(values, last<<8)
+	}
+	calcedSum := fletcher32(values)
+	if calcedSum != checksum {
+		logger.Error("calced sum=", calcedSum, "file sum=", checksum)
+		thrower.Throw(ErrFletcherChecksum)
+	}
+	return newResetReaderFromBytes(b)
 }
 
 func (r *resetReader) Rem() int64 {
@@ -165,4 +211,49 @@ func (rcf *refCountedFile) dereference() error {
 		err = ErrInternal
 	}
 	return err
+}
+
+func newUnshuffleReader(r io.Reader, size uint64, shuffleParam uint32) remReader {
+	return newResetReader(&unshuffleReader{r, nil, size, shuffleParam}, int64(size))
+}
+
+func unshuffle(val []byte, n uint32) {
+	if n == 1 {
+		return // avoids allocation
+	}
+	// inefficent algorithm because it allocates data
+	tmp := make([]byte, len(val))
+	nelems := len(val) / int(n)
+	for i := 0; i < int(n); i++ {
+		for j := 0; j < nelems; j++ {
+			tmp[j*int(n)+i] = val[i*nelems+j]
+		}
+	}
+	copy(val, tmp)
+}
+
+func (r *unshuffleReader) Read(p []byte) (int, error) {
+	if r.size == 0 {
+		return 0, io.EOF
+	}
+	thisLen := uint64(len(p))
+	if thisLen > r.size {
+		thisLen = r.size
+	}
+	var err error
+	if r.b == nil {
+		r.b = make([]byte, r.size)
+		tot, err := readAll(r.r, r.b)
+		unshuffle(r.b[:tot], r.shuffleParam)
+		if err != nil {
+			assert(err == io.EOF, err.Error())
+		}
+	}
+	copy(p, r.b[:thisLen])
+	r.b = r.b[thisLen:]
+	r.size -= thisLen
+	if r.size == 0 {
+		return int(thisLen), io.EOF
+	}
+	return int(thisLen), err
 }
